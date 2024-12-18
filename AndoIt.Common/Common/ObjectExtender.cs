5d8481d964898365631b683b85051ea07576feaa
ANDONI RIPOLL JARAUTA <andoni.ripolljarauta.ext@telefonica.com>
2024-12-18 17:19:51 +0100

    Logs y Limpiar

---
+++ AndoIt.Common/Common/ObjectExtender.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace AndoIt.Common.Core.Common
+{
+    public static class ObjectExtender
+    {
+        public static string ObjectToJsonString(this object extended) {
+            string result = JsonConvert.SerializeObject(extended);
+            return result;
+        }
+    }
+}
